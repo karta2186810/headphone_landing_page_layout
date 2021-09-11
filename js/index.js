e5bf5af7e123ee3dd51ffab6b83a23c2ae26517b
karta2186810 <karta2186810@gmail.com>
2021-09-11 21:00:43 +0800

    style: 完成 discount

--- js/index.js
+++ js/index.js
@@ -25,9 +25,13 @@ window.addEventListener('DOMContentLoaded', () => {
   navLinks.forEach(link => link.addEventListener('click', linkAction))
 
   /* CHANGE BACKGROUND HEADER */
-  function scrollHeader(){
+  function scrollHeader () {
     const header = document.getElementById('header')
-    if(this.scrollY >= 50) header.classList.add('scroll-header'); else header.classList.remove('scroll-header')
+    if(this.scrollY >= 50) {
+      header.classList.add('scroll-header')
+    } else {
+      header.classList.remove('scroll-header')
+    }
   }
   window.addEventListener('scroll', scrollHeader)
 })
